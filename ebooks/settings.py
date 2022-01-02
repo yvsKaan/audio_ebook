48f86fa2c147ede657b617a94b3bc2dfc5e9cc0b
yvsKaan <yavas.kaann@gmail.com>
2022-01-02 15:57:44 +0300

    heroku

--- ebooks/settings.py
+++ ebooks/settings.py
@@ -118,3 +118,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+# heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
