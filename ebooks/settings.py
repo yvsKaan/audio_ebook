3edd3b3f6b07a37016f170bd6aaebc7d86ef0232
yvsKaan <yavas.kaann@gmail.com>
2022-01-02 18:14:58 +0300

    heroku setting.py

--- ebooks/settings.py
+++ ebooks/settings.py
@@ -117,7 +117,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # heroku
-STATIC_ROOT = os.path.join(BASE_DIR, '/audio_ebook/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'audio_ebook/static/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
