3f7f4f8d5c8af1d897c9c25d870a74a43c6e860d
yvsKaan <yavas.kaann@gmail.com>
2022-01-03 13:45:24 +0300

    unsubscribe added

--- audio_ebook/views.py
+++ audio_ebook/views.py
@@ -39,9 +39,10 @@ class Home(ListView):
             return redirect('login')
 
 class CatalogList(View):
-    queryset = Catalog.objects.all()
+    
     def get(self, request):
         if request.user.is_authenticated:
+            queryset = Catalog.objects.all()
             user_subscribe = Subscribe.objects.filter(username= request.user.id)
             search = request.GET.get("search", None)
             if search:
@@ -63,7 +64,7 @@ class CatalogList(View):
                 return render(request, "index.html", context)
             else: 
                 context = {
-                    "catalog_list": self.queryset,
+                    "catalog_list": queryset,
                     'user_subsribe': user_subscribe
                 }
                 return render(request, "cataloglist.html", context)
@@ -76,15 +77,11 @@ class CatalogList(View):
             Q(username= request.POST['username'])&
             Q(catalog= request.POST['catalog'])
             )
-        print(check.exists())
         if form.is_valid() and check.exists() is False:
             form.save()
             return redirect('/')
  
-        context = {
-            "catalog_list": self.queryset
-        }
-        return render(request, "cataloglist.html", context)
+        return redirect('catalogs')
     
 
 
@@ -130,10 +127,9 @@ class BookView(DetailView):
 
 
 class Library(View):
-    model = Subscribe
     def get(self, request):
         if request.user.is_authenticated:
-            queryset = self.model.objects.filter(username = request.user.id)
+            queryset = Subscribe.objects.filter(username = request.user.id)
             search = request.GET.get("search", "")
             if search:
                 catalog_queryset = Catalog.objects.filter(
@@ -159,6 +155,16 @@ class Library(View):
                 return render(request, "library.html", context)
         else: 
             return redirect('login')
+    
+    def post(self, request):
+        form = SubscribeForm(request.POST)
+        check = Subscribe.objects.filter(
+            Q(username= request.POST['username'])&
+            Q(catalog= request.POST['catalog'])
+            )
+        if form.is_valid() and check.exists() is True:
+            check.delete()
+            return redirect('library')
     
 class RegisterFormView(FormView):
     template_name = "registration/register.html"
